7006120bc25b8a46020c1e3729c96944182d95c8
AdnanAzem <adnan.azem.250@gmail.com>
2025-05-04 15:45:50 +0300

    Defining App Rouates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import './index.css'
+import React from "react";
+import "./index.css";
+import { Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import Dashboard from "./pages/Home/Dashboard";
+import EditResume from "./pages/ResumeUpdate/EditResume";
+import LandingPage from "./pages/LandingPage";
+
 
 const App = () => {
   return (
-    <div >App</div>
-  )
-}
+    <>
+      <div>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/resume/:resumeId" element={<EditResume />} />
+        </Routes>
+      </div>
+      <Toaster 
+        roastOptions={{
+          className: "",
+          style: {
+            fontSize: "13px",
+          }
+        }}
+      />
+    </>
+  );
+};
 
-export default App
+export default App;
