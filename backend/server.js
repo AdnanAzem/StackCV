cad039e849440f1b3c0d6964afa20e98247a3eca
AdnanAzem <adnan.azem.250@gmail.com>
2025-05-05 10:42:15 +0300

    Creating Authentication APIs

--- backend/server.js
+++ backend/server.js
@@ -4,17 +4,19 @@ const cors = require("cors");
 const path = require("path");
 const connectDB = require("./config/db");
 
+const authRoutes = require("./routes/authRoutes");
+// const resumeRoutes = require("./routes/resumeRoutes");
+
 const app = express();
 
-
 // middleware to handle cors
-app.use(cors(
-    {
-        origin: process.env.CLIENT_URL || "*",
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        allowedHeaders: ["Content-Type", "Authorization"]
-    }
-));
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 // connect to database
 connectDB();
@@ -22,8 +24,22 @@ connectDB();
 // middleware
 app.use(express.json());
 
+// routes
+app.use("/api/auth", authRoutes);
+// app.use("/api/resume", resumeRoutes);
+
+// Serve uploads folder
+app.use(
+  "/uploads",
+  express.static(path.join(__dirname, "uploads"), {
+    setHeaders: (res, path) => {
+      res.set("Access-Control-Allow-Origin", "http://localhost:5173");
+    },
+  })
+);
+
 //start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
