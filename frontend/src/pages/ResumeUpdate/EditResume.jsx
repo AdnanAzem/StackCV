e8ec1bc17de71d47f58bb883575198039cdfe6f5
AdnanAzem <adnan.azem.250@gmail.com>
2025-05-06 12:58:54 +0300

    Fetching Resume by ID

--- frontend/src/pages/ResumeUpdate/EditResume.jsx
+++ frontend/src/pages/ResumeUpdate/EditResume.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import {
   LuArrowLeft,
@@ -12,13 +12,17 @@ import {
 import toast from "react-hot-toast";
 import DashboardLayout from "../../components/layouts/DashboardLayout";
 import TitleInput from "../../components/Inputs/TitleInput";
-import {useReactToPrint} from 'react-to-print';
+import { useReactToPrint } from "react-to-print";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPaths";
+import StepProgress from "../../components/StepProgress";
+import ProfileInfoForm from "./Forms/ProfileInfoForm";
 
 const EditResume = () => {
   const { resumeId } = useParams();
   const navigate = useNavigate();
 
-  const resumerRef = useRef(null);
+  const resumeRef = useRef(null);
   const resumeDownloadRef = useRef(null);
 
   const [baseWidth, setBaseWidth] = useState(800);
@@ -94,50 +98,98 @@ const EditResume = () => {
   const [isLoading, setIsLoading] = useState(false);
 
   // Validate Inputs
-  const validateAndNext = (e) => {
-    
-  }
+  const validateAndNext = (e) => {};
 
   // Function to navigate to the next page
   const goToNextStep = () => {};
 
   // Function to navigate to the previous page
-  const goToPrevStep = () => {};
+  const goBack = () => {};
 
-  const renderForm = () => {};
+  const renderForm = () => {
+    switch (currentPage) {
+      case "profile-info":
+        return (
+          <ProfileInfoForm
+            profileData={resumeData?.profileInfo}
+            updateSection={(key, value) =>
+              updateSection("profileInfo", key, value)
+            }
+            onNext={validateAndNext}
+          />
+        );
+
+      default:
+        return null;
+    }
+  };
 
   // Update simple nested object (like profileInfo, contactInfo, etc.)
-  const updateSection = (section, key ,value) => {};
+  const updateSection = (section, key, value) => {
+    setResumeData((prev) => ({
+      ...prev,
+      [section]: {
+        ...prev[section],
+        [key]: value,
+      },
+    }));
+  };
 
   //Update array item (like workExperience[0], skills[1], etc.)
   const updateArrayItem = (section, index, key, value) => {};
 
   // Add item to array
-  const addArrayItem = (section, newItem) => {}
+  const addArrayItem = (section, newItem) => {};
 
   //Remove item from array
-  const removeArrayItem = (section, index) => {}  
+  const removeArrayItem = (section, index) => {};
 
   //Fetch resume info by ID
   const fetchResumeDetailsById = async () => {
+    try {
+      const response = await axiosInstance.get(
+        API_PATHS.RESUME.GET_BY_ID(resumeId)
+      );
 
-  }
+      if (response.data && response.data.profileInfo) {
+        const resumeInfo = response.data;
+        setResumeData((prevState) => ({
+          ...prevState,
+          title: resumeInfo?.title || "Untitled",
+          template: resumeInfo?.template || prevState?.template,
+          profileInfo: resumeInfo?.profileInfo || prevState?.profileInfo,
+          contactInfo: resumeInfo?.contactInfo || prevState?.contactInfo,
+          workExperience:
+            resumeInfo?.workExperience || prevState?.workExperience,
+          education: resumeInfo?.education || prevState?.education,
+          skills: resumeInfo?.skills || prevState?.skills,
+          projects: resumeInfo?.projects || prevState?.projects,
+          certifications:
+            resumeInfo?.certifications || prevState?.certifications,
+          languages: resumeInfo?.languages || prevState?.languages,
+          interests: resumeInfo?.interests || prevState?.interests,
+        }));
+      }
+    } catch (error) {
+      console.log("Error fetching resumes:", error);
+    }
+  };
 
   // Upload thumbnail and resume profile img
-  const uploadResumeImages = async () => {} 
+  const uploadResumeImages = async () => {};
 
-  const updateResumeDetails = async (thumbnailLink, profilePreviewUrl) => {}
+  const updateResumeDetails = async (thumbnailLink, profilePreviewUrl) => {};
 
   // Delete Resume
-  const handleDeleteResume = async () => {}
+  const handleDeleteResume = async () => {};
 
   // Download Resume
   const reactToPrintFn = useReactToPrint({
-    contentRef: resumeDownloadRef
+    contentRef: resumeDownloadRef,
   });
 
   // Function to update baseWidth based on the resume container size
-  const updateBaseWidth = () => {}
+  const updateBaseWidth = () => {};
 
   useEffect(() => {
     updateBaseWidth();
@@ -148,9 +200,9 @@ const EditResume = () => {
     }
     return () => {
       window.removeEventListener("resize", updateBaseWidth);
-    }
+    };
   }, []);
-  
+
   return (
     <DashboardLayout>
       <div className="container mx-auto">
@@ -161,6 +213,80 @@ const EditResume = () => {
               setResumeData((prevState) => ({ ...prevState, title: value }))
             }
           />
+
+          <div className="flex items-center gap-4">
+            <button
+              className="btn-small-light"
+              onClick={() => setOpenThemeSelector(true)}
+            >
+              <LuPalette className="text-[16px]" />
+              <span className="hidden md:block">Change Theme</span>
+            </button>
+            <button className="btn-small-light" onClick={handleDeleteResume}>
+              <LuTrash2 className="text-[16px]" />
+              <span className="">Delete</span>
+            </button>
+            <button
+              className="btn-small-light"
+              onClick={() => setOpenPreviewModal(true)}
+            >
+              <LuDownload className="text-[16px]" />
+              <span className="hidden md:block">Preview & Download</span>
+            </button>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+          <div className="bg-white rounded-lg border border-purple-100 overflow-hidden">
+            <StepProgress progress={progress} />
+
+            {renderForm()}
+            <div className="mx-5">
+              {errorMsg && (
+                <div className="flex items-center gap-2 text-[11px] font-medium text-amber-600 bg-amber-100 px-2 py-0.5 my-1 rounded">
+                  <LuCircleAlert className="text-md" /> {errorMsg}
+                </div>
+              )}
+              <div className="flex items-center justify-end gap-3 mt-3 mb-5">
+                <button
+                  className="btn-small-light"
+                  onClick={goBack}
+                  disabled={isLoading}
+                >
+                  <LuArrowLeft className="text-[16px]" /> Back
+                </button>
+
+                <button
+                  className="btn-small-light"
+                  onClick={uploadResumeImages}
+                  disabled={isLoading}
+                >
+                  <LuSave className="text-[16px]" />{" "}
+                  {isLoading ? "Updating..." : "Save & Exit"}
+                </button>
+
+                <button
+                  className="btn-small"
+                  onClick={validateAndNext}
+                  disabled={isLoading}
+                >
+                  {currentPage === "additionalInfo" && (
+                    <LuDownload className="text-[16px]" />
+                  )}
+                  {currentPage === "additionalInfo"
+                    ? "Preview & Download"
+                    : "Next"}
+                  {currentPage !== "additionalInfo" && (
+                    <LuArrowLeft className="text-[16px] rotate-180" />
+                  )}
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div className="h-[100vh]" ref={resumeRef}>
+            {/* Resume Template */}
+          </div>
         </div>
       </div>
     </DashboardLayout>
