af90a2562bee122c96ea120e747cf91d428b8196
AdnanAzem <adnan.azem.250@gmail.com>
2025-05-13 14:34:41 +0300

    import dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
-require("dotenv").config();
+import dotenv from 'dotenv';
+dotenv.config({ path: './.env' });
+
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
